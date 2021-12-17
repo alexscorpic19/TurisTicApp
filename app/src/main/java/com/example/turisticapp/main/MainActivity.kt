1a327ff472aa4fdeaf3e38237d81a839c7df2d90
[alex] <[alxscorpic19@gmail.com]>
2021-12-17 18:26:59 -0500

    se agreaga el fragment preference,la navegacion  y su funcionalidad

--- app/src/main/java/com/example/turisticapp/main/MainActivity.kt
+++ app/src/main/java/com/example/turisticapp/main/MainActivity.kt
@@ -9,12 +9,37 @@ import androidx.appcompat.app.ActionBar
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.turisticapp.R
+import com.example.turisticapp.preference.SettingsFragment
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.overflow_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val fm : FragmentManager = supportFragmentManager
+        val ft : FragmentTransaction = fm.beginTransaction()
+
+        return when (item.itemId){
+            R.id.menu_preferences -> {
+                val settingsFragment = SettingsFragment()
+                ft.replace(R.id.fragmentContainerView, settingsFragment).commit()
+                ft.addToBackStack(null)
+                true
+            }
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> {return true}
+        }
     }
 
 
---
+++ app/src/main/java/com/example/turisticapp/preference/SettingsFragment.kt
@@ -0,0 +1,27 @@
+package com.example.turisticapp.preference
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.preference.PreferenceFragmentCompat
+import com.example.turisticapp.R
+import com.example.turisticapp.main.MainActivity
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        menu.clear()
+    }
+
+}
