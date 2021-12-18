a30d669d953571fb523abf3d8b8501fba8ac4a87
[alex] <[alxscorpic19@gmail.com]>
2021-12-18 03:10:53 -0500

    se agregan los sitios en el mapa

--- app/src/main/java/com/example/turisticapp/maps/MapsFragment.kt
+++ app/src/main/java/com/example/turisticapp/maps/MapsFragment.kt
@@ -17,18 +17,32 @@ import com.google.android.gms.maps.model.MarkerOptions
 class MapsFragment : Fragment() {
 
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        val Bmanga = LatLng(7.12539, -73.1198)
+        googleMap.addMarker(MarkerOptions().position(Bmanga).title("Bucaramanga"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Bmanga, 10F))
+
+        val CatGiron = LatLng(7.0678621529787256, -73.17006689167818)
+        googleMap.addMarker(MarkerOptions().position(CatGiron).title("Catedral de Giron"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CatGiron, 10F))
+
+        val ParkSpio = LatLng(7.118693386891463, -73.11038265984669)
+        googleMap.addMarker(MarkerOptions().position(ParkSpio).title("Parque Sanpio"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ParkSpio, 10F))
+
+        val CasaBlvar = LatLng(7.1170827951049604, -73.12866474635493)
+        googleMap.addMarker(MarkerOptions().position(CasaBlvar).title("Casa de Bolivar"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CasaBlvar, 10F))
+
+        val CerroSt = LatLng(7.081059711953229, -73.07014253101062)
+        googleMap.addMarker(MarkerOptions().position(CerroSt).title("Cerro el santisimo"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(CerroSt, 10F))
+
+        val panachi = LatLng(6.790339953582724, -73.00354945350466)
+        googleMap.addMarker(MarkerOptions().position(panachi).title("PANACHI"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(panachi, 10F))
+
+
     }
 
     override fun onCreateView(
